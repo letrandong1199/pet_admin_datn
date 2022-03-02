08e0a8bb4bf92e955b58a516843048e3e9ab858e
Le Tran Dong <letrandong1199@gmail.com>
2022-03-02 10:38:44 +0700

    fix ui signin

--- src/pages/index.js
+++ src/pages/index.js
@@ -25,77 +25,76 @@ const Login = () => {
       login(email);
     }
   }
-}
-const handleChangeUsername = (e) => {
-  setUsername(e.target.value);
-}
+  const handleChangeUsername = (e) => {
+    setUsername(e.target.value);
+  }
 
-return (
-  <>
-    <Head>
-      <title>Login | Pet-Friends Social</title>
-    </Head>
-    <Box
-      component="main"
-      sx={{
-        alignItems: 'center',
-        display: 'flex',
-        flexGrow: 1,
-        minHeight: '100%'
-      }}
-    >
-      <Container maxWidth="sm">
-        <form onSubmit={handleLogin}>
-          <Box sx={{ my: 3 }}>
-            <Typography
-              color="textPrimary"
-              variant="h4"
+  return (
+    <>
+      <Head>
+        <title>Login | Pet-Friends Social</title>
+      </Head>
+      <Box
+        component="main"
+        sx={{
+          alignItems: 'center',
+          display: 'flex',
+          flexGrow: 1,
+          minHeight: '100%'
+        }}
+      >
+        <Container maxWidth="sm">
+          <form onSubmit={handleLogin}>
+            <Box sx={{ my: 3 }}>
+              <Typography
+                color="textPrimary"
+                variant="h4"
+              >
+                Sign in
+              </Typography>
+              <Typography
+                color="textSecondary"
+                gutterBottom
+                variant="body2"
+              >
+                Sign in with admin account
+              </Typography>
+            </Box>
+            <Grid
+              container
+              spacing={3}
             >
-              Sign in
-            </Typography>
-            <Typography
-              color="textSecondary"
-              gutterBottom
-              variant="body2"
-            >
-              Sign in with admin account
-            </Typography>
-          </Box>
-          <Grid
-            container
-            spacing={3}
-          >
-          </Grid>
-          <TextField
-            // error={!validated}
-            fullWidth
-            // helperText={formik.touched.email && formik.errors.email}
-            label={"Email Address"}
-            margin="normal"
-            name="email"
-            // onBlur={formik.handleBlur}
-            onChange={handleChangeUsername}
-            type="email"
-            value={username}
-            variant="outlined"
-          />
-          <Box sx={{ py: 2 }}>
-            <Button
-              color="primary"
-              // disabled={isSubmitting}
+            </Grid>
+            <TextField
+              // error={!validated}
               fullWidth
-              size="large"
-              type="submit"
-              variant="contained"
-            >
-              Sign In Now
-            </Button>
-          </Box>
-        </form>
-      </Container>
-    </Box>
-  </>
-);
+              // helperText={formik.touched.email && formik.errors.email}
+              label={"Email Address"}
+              margin="normal"
+              name="email"
+              // onBlur={formik.handleBlur}
+              onChange={handleChangeUsername}
+              type="email"
+              value={username}
+              variant="outlined"
+            />
+            <Box sx={{ py: 2 }}>
+              <Button
+                color="primary"
+                // disabled={isSubmitting}
+                fullWidth
+                size="large"
+                type="submit"
+                variant="contained"
+              >
+                Sign In Now
+              </Button>
+            </Box>
+          </form>
+        </Container>
+      </Box>
+    </>
+  );
 };
 
 export default Login;
